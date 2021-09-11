38141f0481423aea45e4515709ccdb3d90ea8abe
Helder Avila <helderavila2016@gmail.com>
2021-09-11 12:28:20 -0300

    Store data on smart contract

--- contracts/WavePortal.sol
+++ contracts/WavePortal.sol
@@ -4,7 +4,19 @@ pragma solidity ^0.8.4;
 import "hardhat/console.sol";
 
 contract WavePortal {
+  uint totalWaves;
+
   constructor() {
     console.log("Hello World!");
+  }
+
+  function wave() public {
+    totalWaves += 1;
+    console.log("%s is waved", msg.sender);
+  }
+
+  function getTotalWaves() view public returns (uint) {
+    console.log("We have %d total waves", totalWaves);
+    return totalWaves;
   }
 }
