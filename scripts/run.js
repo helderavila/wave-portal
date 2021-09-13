b9a0bad972fcdce53ae6718e4c0b785ee1dda679
Helder Avila <helderavila2016@gmail.com>
2021-09-13 10:00:55 -0300

    Enviando ether para quem enviar um wave

--- scripts/run.js
+++ scripts/run.js
@@ -1,19 +1,19 @@
 async function main() {
   const [owner, randoPerson] = await ethers.getSigners()
   const waveContractFactory = await hre.ethers.getContractFactory("WavePortal")
-  const waveContract = await waveContractFactory.deploy()
+  const waveContract = await waveContractFactory.deploy({ value: hre.ethers.utils.parseEther("0.1") })
   await waveContract.deployed()
   console.log("Contract deployed to: ", waveContract.address)
   console.log("Contract deployed by: ", owner.address)
 
-  let waveCount
-  waveCount = await waveContract.getTotalWaves()
+  let contractBalance = await hre.ethers.provider.getBalance(waveContract.address)
+  console.log("Contract balance: ", hre.ethers.utils.formatEther(contractBalance))
 
   let waveTxn = await waveContract.wave("Hello World!")
   await waveTxn.wait()
 
-  waveTxn = await waveContract.connect(randoPerson).wave("Another random message!")
-  await waveTxn.wait()
+  contractBalance = await hre.ethers.provider.getBalance(waveContract.address)
+  console.log("Contract balance: ", hre.ethers.utils.formatEther(contractBalance))
 
   let allWaves = await waveContract.getAllWaves()
   console.log(allWaves)
